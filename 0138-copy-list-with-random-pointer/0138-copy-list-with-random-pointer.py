6a12a9bb94dac838af346c725e1b28bf2908823c
Devang Pujare <devangpujare@gmail.com>
2026-09-10 23:52:17 +0530

    Time: 56 ms (8.91%), Space: 19.9 MB (88.48%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        old_to_new = {None: None}
+
+        current = head
+        
+        while current:
+            old_to_new[current] = Node(current.val)
+            current = current.next
+
+        current = head
+
+        while current:
+            old_to_new[current].next = old_to_new[current.next]
+            old_to_new[current].random = old_to_new[current.random]
+
+            current = current.next
+
+        return old_to_new[head]
